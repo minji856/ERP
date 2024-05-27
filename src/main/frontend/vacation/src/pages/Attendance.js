87fd60875c966aa3e8bbf80c65f59e06ea05c55c
minji856 <sistar96@gmail.com>
2024-05-28 02:12:44 +0900

    연도 2자리수로 변경

--- src/main/frontend/vacation/src/pages/Attendance.js
+++ src/main/frontend/vacation/src/pages/Attendance.js
@@ -15,18 +15,19 @@ const Attendance = () => {
         return () => clearInterval(intervalId);
     }, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행되도록 합니다.
     
-    const nowTime = moment().format('YYYY-MM-DD HH:mm');
+    const nowTime = moment().format('YY-MM-DD HH:mm');
     //console.log(nowTime)
 
     const handleClockIn = () => {
         // 출근 기능을 수행할 코드를 추가합니다.
         // 이미 출근된 상태면 출근 버튼 비활성화
-        alert("출근되었습니다.")
+        alert("출근하겠습니까?");
         console.log('출근했습니다.');
     };
 
     const handleClockOut = () => {
         // 퇴근 기능을 수행할 코드를 추가합니다.
+        // 비활성화로 설정해두기
         console.log('퇴근했습니다.');
     };
 
