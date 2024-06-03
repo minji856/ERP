f20ddd9d1139b7679c95c21beb618fb441276817
minji856 <sistar96@gmail.com>
2024-06-03 21:26:16 +0900

    서버로직위치를 handle로 옮김

--- src/main/frontend/vacation/src/pages/Attendance.js
+++ src/main/frontend/vacation/src/pages/Attendance.js
@@ -15,21 +15,6 @@ const Attendance = () => {
     useEffect(() => {
         if (workClickTime) {
             console.log('출근한 시간: ' + workClickTime);
-            // 출근한 시간을 서버로 전송
-            fetch('/api/work-in', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ workClickTime })
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('출근 시간 저장 성공:', data);
-            })
-            .catch((error) => {
-                console.error('출근 시간 저장 실패:', error);
-            });
         }
     }, [workClickTime]);
 
@@ -43,6 +28,20 @@ const Attendance = () => {
     const handleClockIn = () => {
         setworkClickTime(currentTime);
         if(window.confirm("출근하시겠습니까?")){
+            fetch('/api/work-in', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ workClickTime: currentTime })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('출근 시간 저장 성공:', data);
+            })
+            .catch((error) => {
+                console.error('출근 시간 저장 실패:', error);
+            });
             setisleaveWorkdisabled(false); // 퇴근 버튼이 활성화됨
             setisWorkdisabled(true); // 출근 버튼이 비활성화됨
         }
