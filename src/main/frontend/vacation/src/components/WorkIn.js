ddd30eb44857eacec9fef295eb4f26f6516818e9
minji856 <sistar96@gmail.com>
2024-06-04 23:17:43 +0900

    출근기능 분리

---
+++ src/main/frontend/vacation/src/components/WorkIn.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const WorkIn = () => {
+    if(window.confirm("출근하시겠습니까?")){
+        fetch('/api/work-in', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ workClickTime: currentTime })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('출근 시간 저장 성공:', data);
+        })
+        .catch((error) => {
+            console.error('출근 시간 저장 실패:', error);
+        });
+        setisleaveWorkdisabled(false); // 퇴근 버튼이 활성화됨
+        setisWorkdisabled(true); // 출근 버튼이 비활성화됨
+    }
+}
+
+export default WorkIn;
