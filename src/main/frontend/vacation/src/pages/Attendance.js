b7d96f2e25113d6b35d9ef858b6ec8e84e031223
minji856 <sistar96@gmail.com>
2024-06-04 23:34:59 +0900

    기능은 우선 문제 X

--- src/main/frontend/vacation/src/pages/Attendance.js
+++ src/main/frontend/vacation/src/pages/Attendance.js
@@ -1,49 +1,15 @@
 import React , { useState }from 'react';
 // import moment from 'moment';
-// 자동으로 한국 시간을 불러오지만 import 추가 함
 import 'moment/locale/ko';
-import WorkIn from 'components/WorkIn';
+import { handleClockIn, handleClockOut} from 'components/WorkIn';
 
 const Attendance = () => {
-    // moment.js를 이용한 시간 출력
     const [workClickTime, setworkClickTime] = useState(null);
     const [leaveWorkClickTime, setleaveWorkClickTime] = useState(null);
     const [isWorkdisabled, setisWorkdisabled] = useState(false);
     const [isleaveWorkdisabled, setisleaveWorkdisabled] = useState(true);
     const currentTime = new Date().toLocaleString();
     
-    /* 출근시간을 저장하는 메서드입니다. */
-    const handleClockIn = () => {
-        setworkClickTime(currentTime);
-        if(window.confirm("출근하시겠습니까?")){
-            fetch('/api/work-in', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ workClickTime: currentTime })
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('출근 시간 저장 성공:', data);
-            })
-            .catch((error) => {
-                console.error('출근 시간 저장 실패:', error);
-            });
-            setisleaveWorkdisabled(false); // 퇴근 버튼이 활성화됨
-            setisWorkdisabled(true); // 출근 버튼이 비활성화됨
-        }
-    };
-    
-    /* 퇴근시간을 저장하는 메서드입니다. */
-    const handleClockOut = () => {
-        setleaveWorkClickTime(currentTime);
-        if(window.confirm("퇴근하시겠습니까?")){
-            setisleaveWorkdisabled(true); // 퇴근 버튼이 비활성화됨
-            setisWorkdisabled(false); // 출근 버튼이 다시 활성화됨
-        }
-    };
-
     return (
         <>
         {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
@@ -58,9 +24,9 @@ const Attendance = () => {
                         <header>
                             <div>
                                 <h1>현재 시간: {new Date().toLocaleString()}</h1>
-                                <button onClick={handleClockIn} disabled={isWorkdisabled}>출근하기</button><br/>
+                                <button onClick={() => handleClockIn(currentTime, setworkClickTime, setisWorkdisabled, setisleaveWorkdisabled)} disabled={isWorkdisabled}>출근하기</button><br/>
                                 <button>외근</button><br/>
-                                <button onClick={handleClockOut} disabled={isleaveWorkdisabled}>퇴근하기</button>
+                                <button onClick={() => handleClockOut(currentTime, setleaveWorkClickTime, setisWorkdisabled, setisleaveWorkdisabled)} disabled={isleaveWorkdisabled}>퇴근하기</button>
                             </div>
                         </header>
                     </div>
