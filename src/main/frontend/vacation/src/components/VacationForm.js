b67ebb29b5f561ab1472ada581f03755a53cfc68
minji856 <sistar96@gmail.com>
2024-06-17 22:48:16 +0900

    Fea 휴가신청 초기화 기능 완성

--- src/main/frontend/vacation/src/components/VacationForm.js
+++ src/main/frontend/vacation/src/components/VacationForm.js
@@ -1,4 +1,4 @@
-import React , { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 /**
@@ -7,78 +7,71 @@ import axios from 'axios';
  * @param setFormData 저장된 입력값을 담은 변수
  */
 const VacationForm = () => {
-  const [vacationType, setVacationType] = useState('');
-  const [holidayStart, setHolidayStart] = useState('');
-  const [holidayEnd, setHolidayEnd] = useState('');
-  const [reason, setReason] = useState('');
-
-  const handleType = (event) => {
-    event.preventDefault();
-    setVacationType(event.target.value);
+  const initialFormState = {
+    vacationType: '',
+    holidayStart: '',
+    holidayEnd: '',
+    reason: ''
   };
 
-  const handleHolidayStart = (event) => {
-    event.preventDefault();
-    setHolidayStart(event.target.value);
-  };
+  const [formData, setFormData] = useState(initialFormState);
 
-  const handleHolidayEnd = (event) => {
-    event.preventDefault();
-    setHolidayEnd(event.target.value);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
   };
-
-  const handleReason = (event) => {
-    event.preventDefault();
-    setReason(event.target.value);
-  };
-
-  // const handleChange = e => {
-  //   setFormData({
-  //     ...formData,
-  //     [e.target.name]: e.target.value,
-  //   })
-  // }
 
   const submitHandler = (event) => {
     event.preventDefault();
-    const formData = new FormData();
-    // alert(JSON.stringify(formData, null, 2));
-    
-    formData.append("vacationType", vacationType);
-    formData.append("holidayStart", holidayStart);
-    formData.append("holidayEnd", holidayEnd);
-    formData.append("reason", reason);
 
-    // formData 객체가 제대로 만들어졌는지 확인
-    console.log(vacationType, holidayStart, holidayEnd, reason);
+    if (formData.vacationType === 'error') {
+      alert('휴가 구분을 선택해주세요.');
+      return;
+    }
+
+    const data = new FormData();
+    data.append("vacationType", formData.vacationType);
+    data.append("holidayStart", formData.holidayStart);
+    data.append("holidayEnd", formData.holidayEnd);
+    data.append("reason", formData.reason);
 
     axios
-      .post('http://localhost:8484/api/vacation', formData) 
+      .post('http://localhost:8484/api/vacation', data)
       .then((res) => {
         console.log("데이터가 전송되었습니다.");
         console.log(res);
       })
       .catch((err) => {
-        console.log("에러 발생 : "+ err);
-     });
-  }
+        console.log("에러 발생 : " + err);
+      });
+  };
 
   /**
    * 신청취소를 누르면 예 누를시 작성한 데이터가 초기화됩니다.
    */
   const handleReset = () => {
-    window.confirm('신청을 취소하시겠습니까? 예 를 누를시 입력한 내용이 삭제됩니다.')
-  }
+    if (window.confirm('신청을 취소하시겠습니까? 예 를 누를시 입력한 내용이 삭제됩니다.')) {
+      setFormData(initialFormState);
+    }
+  };
 
   return (
     <form className="vacationForm" onSubmit={submitHandler} method="post" action="/api/vacRequest">
-      <input type="reset" value="신청취소" onClick={handleReset}/>
+      <button type="button" onClick={handleReset}>신청취소</button>
       <table>
-        <tr>
-          <td>
+        <tbody>
+          <tr>
+            <td>
               휴가구분
-              <select className="vacationType" required onChange={handleType}>
-                {/* <!-- -선택- 눌렀을 때 정보 다시 입력 알림 창 띄우기 --> */}
+              <select
+                name="vacationType"
+                value={formData.vacationType}
+                required
+                onChange={handleChange}
+              >
                 <option value="error"> —선택— </option>
                 <option value="MonthlyLeave"> 월차 </option>
                 <option value="HalfDay"> 반차 </option>
@@ -88,23 +81,39 @@ const VacationForm = () => {
                 <option value="Other"> 기타 </option>
               </select>
             </td>
-        </tr>
-        <tr>
-          <td>휴가 기간
-            <input type="date" name="holidayStart" required onChange={handleHolidayStart}/> ~
-            <input type="date" name="holidayEnd" required onChange={handleHolidayEnd}/>
-          </td>
-      </tr>
-      <tr>
-      </tr>
-      <tr>
-      </tr>
-        <td colSpan='3'>
-          (비고)사유 : <textarea name="reason"
-            placeholder="휴가 사유를 입력하세요."
-            required 
-            onChange={handleReason}></textarea>
-        </td>
+          </tr>
+          <tr>
+            <td>
+              휴가 기간
+              <input
+                type="date"
+                name="holidayStart"
+                value={formData.holidayStart}
+                required
+                onChange={handleChange}
+              /> ~
+              <input
+                type="date"
+                name="holidayEnd"
+                value={formData.holidayEnd}
+                required
+                onChange={handleChange}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="3">
+              (비고)사유 : 
+              <textarea
+                name="reason"
+                placeholder="휴가 사유를 입력하세요."
+                value={formData.reason}
+                required
+                onChange={handleChange}
+              ></textarea>
+            </td>
+          </tr>
+        </tbody>
       </table>
       <input type="submit" value="신청완료" />
     </form>
--- src/main/frontend/vacation/src/pages/Attendance.js
+++ src/main/frontend/vacation/src/pages/Attendance.js
@@ -1,63 +1,64 @@
-// import React , { useState }from 'react';
-// import WorkIn from 'components/WorkIn';
-// import WorkOut from 'components/WorkOut';
-// import NowClock from 'components/NowClock';
-// import OnlyNumClock from 'components/OnlyNumClock';
+import React , { useState }from 'react';
+import WorkIn from 'components/WorkIn';
+import WorkOut from 'components/WorkOut';
+import NowClock from 'components/NowClock';
+import OnlyNumClock from 'components/OnlyNumClock';
 
-// const Attendance = () => {
-//     const [workClickTime, setworkClickTime] = useState(null);
-//     const [leaveWorkClickTime, setleaveWorkClickTime] = useState(null);
-//     const [isWorkdisabled, setisWorkdisabled] = useState(false);
-//     const [isleaveWorkdisabled, setisleaveWorkdisabled] = useState(true);
-//     const currentTime = new Date().toLocaleString();
+const Attendance = () => {
+    const [workClickTime, setworkClickTime] = useState(null);
+    const [leaveWorkClickTime, setleaveWorkClickTime] = useState(null);
+    const [isWorkdisabled, setisWorkdisabled] = useState(false);
+    const [isleaveWorkdisabled, setisleaveWorkdisabled] = useState(true);
+    const currentTime = new Date().toLocaleString();
     
-//     return (
-//         <>
-//         {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
-//         <div className="container mt-5">
-//             <h1 className="h3 mb-0 text-gray-800">출퇴근</h1>
-//             <div className="card mt-4">
-//                 <div className="card-header">
-//                     <h5>근태관리</h5>
-//                 </div>
-//                 <div className="card-body">
-//                     <div className="col-md-12">
-//                         <header>
-//                             <div>
-//                                 <h1>현재 시간 : <NowClock/></h1>
-//                             </div>
-//                             <br></br>
-//                             <br></br>
-//                             <br></br>
-//                             <h3>출근 시간 퇴근 시간</h3>
-//                             <br></br>
-//                             <br></br>
-//                             <br></br>
-//                             {/* 공백 출력 <p>금붕어<Span />|<Span />과장</p> */}
-//                             <h1><OnlyNumClock/></h1>
-//                             <div>
-//                                 <button onClick={() => 
-//                                     WorkIn(currentTime, workClickTime, setworkClickTime, setisWorkdisabled, setisleaveWorkdisabled)} 
-//                                     disabled={isWorkdisabled}>출근하기<br></br><OnlyNumClock/>
-//                                 </button>
-//                                 <button>외근</button>
-//                                 <button onClick={() => 
-//                                     WorkOut(currentTime, leaveWorkClickTime, setleaveWorkClickTime, setisWorkdisabled, setisleaveWorkdisabled)} 
-//                                     disabled={isleaveWorkdisabled}>퇴근하기
-//                                 </button>
-//                             </div>
-//                         </header>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//         </>
-//     );
-// }
+    return (
+        <>
+        {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
+        <div className="container mt-5">
+            <h1 className="h3 mb-0 text-gray-800">출퇴근</h1>
+            <div className="card mt-4">
+                <div className="card-header">
+                    <h5>근태관리</h5>
+                </div>
+                <div className="card-body">
+                    <div className="col-md-12">
+                        <header>
+                            <div>
+                                <h1>현재 시간 : <NowClock/></h1>
+                            </div>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <h3>출근 시간 퇴근 시간</h3>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            {/* 공백 출력 <p>금붕어<Span />|<Span />과장</p> */}
+                            <h1><OnlyNumClock/></h1>
+                            <div>
+                                <button onClick={() => 
+                                    WorkIn(currentTime, workClickTime, setworkClickTime, setisWorkdisabled, setisleaveWorkdisabled)} 
+                                    disabled={isWorkdisabled}>출근하기<br></br><OnlyNumClock/>
+                                </button>
+                                <button>외근</button>
+                                <button onClick={() => 
+                                    WorkOut(currentTime, leaveWorkClickTime, setleaveWorkClickTime, setisWorkdisabled, setisleaveWorkdisabled)} 
+                                    disabled={isleaveWorkdisabled}>퇴근하기
+                                </button>
+                            </div>
+                        </header>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
 
-// export default Attendance;
+export default Attendance;
 
 
+/* 모달창 버전 
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import WorkIn from 'components/WorkIn';
@@ -84,7 +85,7 @@ const Attendance = () => {
 
     return (
         <>
-        {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
+        {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 }
         <div className="container mt-5">
             <h1 className="h3 mb-0 text-gray-800">출퇴근</h1>
             <div className="card mt-4">
@@ -136,3 +137,4 @@ const Attendance = () => {
 }
 
 export default Attendance;
+*/
